1bf65f82ff6c4f9dd9f88a54bf42e32c54f46bb7
Andrei Mashedo <ahdma@mail.ru>
2022-12-15 01:55:24 +0300

    feat: add main.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import './global.scss';
 //import 'bootstrap';
 //import 'bootstrap/dist/css/bootstrap.min.css';
+import './global.scss';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as bootstrap from 'bootstrap';
