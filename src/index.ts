da9bcf5af4ec53e9fa151a205576180f43bf0ccc
Andrei Mashedo <ahdma@mail.ru>
2022-12-18 19:52:24 +0300

    feat: add react

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
-import ItemDescription from './components/description/description';
+//import LeftBar from './components/Store/leftBar';
+//import data from './products.json';
+//import Component from './components/Store/component';
+//import { Range } from './types';
+//import ItemDescription from './components/description/description';
+
+
+import { pageHeader } from 'components/header/header';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './global.scss';
-import LeftBar from './components/Store/leftBar';
-//import 'bootstrap';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-//import * as bootstrap from 'bootstrap';
-import data from './products.json';
-import Component from './components/Store/component';
-import { Range } from './types';
+//const main = new Component(null, [], 'main');
 
-const main = new Component(null, [], 'main');
+//const products = data.products;
 
-const products = data.products;
+//const cats = products.reduce((res: string[], x) => {
+//  if (res.indexOf(x.category) === -1) res.push(x.category);
+//  return res;
+//}, [] as string[]);
 
-const cats = products.reduce((res: string[], x) => {
-  if (res.indexOf(x.category) === -1) res.push(x.category);
-  return res;
-}, [] as string[]);
+//console.log(cats);
 
-console.log(cats);
+//const brands: string[] = [];
+//const price: Range = [1, 100];
+//const stock: Range = [0, 0];
 
-const brands: string[] = [];
-const price: Range = [1, 100];
-const stock: Range = [0, 0];
+//const aside = new LeftBar(main, [], 'aside', cats, brands, price, stock);
+//const asideHtml = document.querySelector('aside');
+//asideHtml!.innerHTML = aside.render();
 
-const aside = new LeftBar(main, [], 'aside', cats, brands, price, stock);
-const asideHtml = document.querySelector('aside');
-asideHtml!.innerHTML = aside.render();
+//const x = new ItemDescription(main, [], 'div', 1);
+//console.log(x.render());
 
-const x = new ItemDescription(main, [], 'div', 1);
-console.log(x.render());
+const root = document.getElementById('root') as unknown;
+ReactDOM.render(, root);
