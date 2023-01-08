5857703c221ff55b4d6c9b227d0564f9fadf7aba
Andrei Mashedo <ahdma@mail.ru>
2023-01-08 15:47:33 +0300

    add 404 page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,10 +3,12 @@ import { StorePage } from '../Pages/StorePage';
 import { DescriptionPage } from '../Pages/DescriptionPage';
 import { Route, Routes } from 'react-router-dom';
 import CartPage from '../Pages/CartPage';
+import { Page404 } from '../Pages/Error';
 
 export const App = () => {
   return (
     <Routes>
+      <Route path="*" element={<Page404 />} />
       <Route path="/about/:id" element={<DescriptionPage />} />
       <Route path="/" element={<StorePage />} />
       <Route path="/cart" element={<CartPage />} />
